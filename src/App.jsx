009c113561f48273cfc360ef8590df884faf970e
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-10-15 22:48:17 -0300

    create basic structure for resume components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+export { Resume };
+
+function Resume() {
+  return (
+    <>
+      <h1>Resume</h1>
+      <div>
+        <form>
+          <h2>General Information</h2>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" id="name" />
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email" id="email" />
+          <label htmlFor="phone">Phone Number:</label>
+          <input type="tel" name="phone" id="phone" />
+          <button type="submit">Submit</button>
+        </form>
+        <form>
+          <h2>Educational Experience</h2>
+          <label htmlFor="school-name">School Name:</label>
+          <input type="text" name="school-name" id="school-name" />
+          <label htmlFor="title">Title Of Study: </label>
+          <input type="text" name="title" id="title" />
+          <label htmlFor="date-school">Date of Study:</label>
+          <input type="date" name="date-school" id="date-school" />
+          <button type="submit">Submit</button>
+        </form>
+        <form>
+          <h2>Practical Experience</h2>
+          <label htmlFor="company-name">Company Name:</label>
+          <input type="text" name="company-name" id="company-name" />
+          <label htmlFor="responsibilities">Main Responsibilities:</label>
+          <textarea name="responsibilities" id="responsibilities"></textarea>
+          <label htmlFor="date-start">Start Date:</label>
+          <input type="date" name="date-start" id="date-start" />
+          <label htmlFor="date-end">End Date:</label>
+          <input type="date" name="date-end" id="date-end" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,7 +72,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
