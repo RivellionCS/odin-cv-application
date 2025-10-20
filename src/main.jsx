990e1ad9dd73440e46c22fde43f6bf0f83b7c18a
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-10-20 20:50:18 -0300

    fix import location issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "../styles/index.css";
+import "./styles/index.css";
 import { GeneralForm } from "./components/General.jsx";
 import { EducationForm } from "./components/Education.jsx";
 import { WorkForm } from "./components/Work.jsx";
